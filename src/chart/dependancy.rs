11e3a8113acc6b9ed598bf825526acd218d1df7e
Guillaume CAMUS <guillaume.camus@manomano.com>
2022-12-08 10:05:13 +0000

    fix: remove unused method

--- src/chart/dependancy.rs
+++ src/chart/dependancy.rs
@@ -14,16 +14,6 @@ impl Dependency {
     pub fn new(source: usize, target: usize) -> Self {
         Self { source, target }
     }
-
-    /// Returns the source of this [`Dependency`].
-    pub fn source(&self) -> usize {
-        self.source
-    }
-
-    /// Returns the target of this [`Dependency`].
-    pub fn target(&self) -> usize {
-        self.target
-    }
 }
 
 #[cfg(test)]
@@ -33,7 +23,7 @@ mod test {
     #[test]
     fn create_dependency() {
         let deps = Dependency::new(4, 6);
-        assert_eq!(4, deps.source());
-        assert_eq!(6, deps.target());
+        assert_eq!(4, deps.source);
+        assert_eq!(6, deps.target);
     }
 }
